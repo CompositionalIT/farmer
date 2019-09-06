6c8b6654bb7c8fa7f81b8f234c10e29c387e053b
Isaac Abraham <isaac.abraham@gmail.com>
2019-09-06 15:40:27 +0200

    sample app

---
+++ src/SampleApp/Program.fs
@@ -0,0 +1,25 @@
+﻿open Farmer
+open Helpers
+
+/// A web app
+let myWebApp = webApp {
+    name (Literal "mysuperwebapp")
+    service_plan_name (Literal "myserverfarm")
+    sku WebApp.Skus.F1
+    use_app_insights (Literal "myappinsights")
+}
+
+/// The overall ARM template which has the app as a resource.
+let template = arm {
+    location Locations.``North Europe``
+    resource myWebApp
+}
+
+// Now export it as an ARM template.
+printf "Writing the ARM template..."
+
+template
+|> Writer.toJson
+|> Writer.toFile @"webapp-appinsights.json"
+
+printfn " all done!"
