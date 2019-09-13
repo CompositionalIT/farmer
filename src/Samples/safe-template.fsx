9017d5858c364c5c8bac62fe8bed3df0d18862f4
Isaac Abraham <isaac.abraham@gmail.com>
2019-09-13 13:27:03 +0100

    Fix sample

--- src/Samples/safe-template.fsx
+++ src/Samples/safe-template.fsx
@@ -2,46 +2,37 @@
 
 open Farmer
 
-let myStorageAccount = storageAccount {
-    name (Variable "storage")
-    sku Storage.Sku.StandardLRS
-}
-
-let web = webApp {
-    name (Variable "web")
-    service_plan_name (Variable "appServicePlan")
-    sku (Parameter "pricingTier")
-
-    use_app_insights (Variable "insights")
-
-    website_node_default_version "8.1.4"
-    setting "public_path" "./public"
-    setting "STORAGE_CONNECTIONSTRING" myStorageAccount.Key
-
-    depends_on myStorageAccount
-}
-
-let template =
-    let withPostfix element = concat [ Variable "prefix"; Literal element ]
-    arm {
-        parameters [ "environment"; "location"; "pricingTier" ]
-
-        variable "environment" (toLower (Parameter "environment"))
-        variable "prefix" (concat [ Literal "safe-"; Variable "environment" ])
-        variable "appServicePlan" (withPostfix "-web-host")
-        variable "web" (withPostfix "-web")
-        variable "storage" (concat [ Literal "safe"; Variable "environment"; Literal "storage" ])
-        variable "insights" (withPostfix "-insights")
-
-        location (Parameter "location")
-
-        resource myStorageAccount
-        resource web
-
-        output "webAppName" web.Name
-        output "webAppPassword" web.PublishingPassword
+let template (environment:string) storageSku webAppSku =
+    let environment = environment.ToLower()
+    let generateResourceName = sprintf "safe-%s-%s" environment 
+    
+    let myStorageAccount = storageAccount {
+        name (sprintf "safe%sstorage" environment)
+        sku storageSku
     }
 
-template
+    let myWebApp = webApp {
+        name (generateResourceName "web")
+        service_plan_name (generateResourceName "webhost")
+        sku webAppSku
+
+        use_app_insights (generateResourceName "insights")
+
+        website_node_default_version "8.1.4"
+        setting "public_path" "./public"
+        setting "STORAGE_CONNECTIONSTRING" myStorageAccount.Key
+
+        depends_on myStorageAccount
+    }
+
+    arm {
+        resource myStorageAccount
+        resource myWebApp
+
+        output "webAppName" myWebApp.Name
+        output "webAppPassword" myWebApp.PublishingPassword        
+    }
+
+template "dev" Storage.Sku.StandardLRS WebApp.Sku.F1
 |> Writer.toJson
 |> Writer.toFile @"safe-template.json"
