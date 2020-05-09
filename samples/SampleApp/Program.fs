8b63776a0058da42127f778ad1ab73b0d61a190f
Isaac Abraham <isaac.abraham@gmail.com>
2020-05-09 18:35:55 +0200

    Revert accidental change

--- samples/SampleApp/Program.fs
+++ samples/SampleApp/Program.fs
@@ -1,41 +1,18 @@
 ﻿open Farmer
-open Farmer.Builders
-open Farmer.ArmBuilder.Helpers
+open Farmer.Resources
 
-let eventGrid (topicName, subscriptionName, subscriptionUrl:string) (location:Location) = [
-   {| name = topicName
-      ``type`` = "Microsoft.EventGrid/topics"
-      location = location.ArmValue
-      apiVersion = "2018-01-01" |} |> toArmResource
-
-   {| name = topicName + "/Microsoft.EventGrid/" + subscriptionName
-      ``type`` = "Microsoft.EventGrid/topics/providers/eventSubscriptions"
-      location = location.ArmValue
-      apiVersion = "2018-01-01"
-      properties =
-          {| destination =
-               {| endpointType = "WebHook"
-                  properties = {| endpointUrl = subscriptionUrl |}
-               |}
-             filter = {| includedEventTypes = [ "All" ] |}
-          |}
-      dependsOn = [ topicName ]
-   |} |> toArmResource
-]
-
-let createEventGrid = eventGrid >> asBuilder
-
-let myEventGrid = createEventGrid ("THE-TOPIC", "THE-SUB", "https://requestb.in/1jz6i2h1")
-
-let storage = storageAccount {
-    name "isaacstorage"
-}
+//TODO: Create resources here!
 
 let deployment = arm {
     location NorthEurope
-    add_resource storage
-    add_resource myEventGrid
+
+    //TODO: Assign resources here using the add_resource keyword
 }
 
+// Generate the ARM template here...
 deployment
 |> Writer.quickWrite @"generated-template"
+
+// Or deploy it directly to Azure here... (required Azure CLI installed!)
+// deployment
+// |> Deploy.quick "my-resource-group"
