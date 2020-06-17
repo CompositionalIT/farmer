837c16da2ab792281efa23dc00fa91bb36b1adc7
Dave Curylo <curylod@asme.org>
2020-06-16 21:21:58 -0400

    Revert Program.fs testbed

--- samples/SampleApp/Program.fs
+++ samples/SampleApp/Program.fs
@@ -3,63 +3,10 @@ open Farmer.Builders
 
 //TODO: Create resources here!
 
-//TODO: Testing out the user experience
-
-let privateNetwork = vnet {
-    name "private-vnet"
-    add_address_spaces [
-        "10.30.0.0/16"
-    ]
-    add_subnets [
-        subnet {
-            name "ContainerSubnet"
-            prefix "10.30.19.0/24"
-            add_delegations [
-                SubnetDelegationService.ContainerGroups
-            ]
-        }
-    ]
-}
-
-let aciProfile = networkProfile {
-    name "vnet-aci-profile"
-    vnet "private-vnet"
-    subnet "ContainerSubnet"
-    // Typically just one subnet is needed
-    // but instead they might want to add many, like this
-    (* add_interface_configs [
-        interface_config {
-            add_ipconfigs [
-                ip_config { subnet "ContainerSubnet" }
-            ]
-        }
-    ]*)
-}
-
-
-
-let myContainer = container {
-    name "container1"
-    image "microsoft/aci-helloworld"
-    network_profile "vnet-aci-profile"
-    ports [ 80us ]
-    // the old member (marked obsolete)
-    //ip_address (ContainerGroup.PrivateAddressWithIp (System.Net.IPAddress.Parse "10.30.19.4")) [TCP, 80us]
-    // prefer one of these
-    public_dns "my-container" [TCP, 80us]
-    private_ip [TCP, 80us]
-    private_static_ip "10.30.19.4" [TCP, 80us]
-}
-
 let deployment = arm {
-    location Location.WestUS
-    
+    location Location.NorthEurope
+
     //TODO: Assign resources here using the add_resource keyword
-    add_resources [
-        myContainer
-        aciProfile
-        privateNetwork
-    ]
 }
 
 // Generate the ARM template here...
