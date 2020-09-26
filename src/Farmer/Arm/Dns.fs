9c79dc217e68cf6cc0885d9ceb8d34fa5273df6f
isaacabraham <isaac.abraham@gmail.com>
2020-09-26 23:41:07 +0100

    Fix build

--- src/Farmer/Arm/Dns.fs
+++ src/Farmer/Arm/Dns.fs
@@ -5,35 +5,20 @@ open Farmer
 open Farmer.CoreTypes
 open Farmer.Dns
 
-let zones = ResourceType "Microsoft.Network/dnszones"
-let aRecord = ResourceType "Microsoft.Network/dnszones/A"
-let aaaaRecord = ResourceType "Microsoft.Network/dnszones/AAAA"
-let cnameRecord = ResourceType "Microsoft.Network/dnszones/CNAME"
-let txtRecord = ResourceType "Microsoft.Network/dnszones/TXT"
-let mxRecord = ResourceType "Microsoft.Network/dnszones/MX"
-let nsRecord = ResourceType "Microsoft.Network/dnszones/NS"
-let soaRecord = ResourceType "Microsoft.Network/dnszones/SOA"
-let srvRecord = ResourceType "Microsoft.Network/dnszones/SRV"
-let ptrRecord = ResourceType "Microsoft.Network/dnszones/PTR"
+let zones = ResourceType ("Microsoft.Network/dnszones", "2018-05-01")
+let aRecord = ResourceType ("Microsoft.Network/dnszones/A", "2018-05-01")
+let aaaaRecord = ResourceType ("Microsoft.Network/dnszones/AAAA", "2018-05-01")
+let cnameRecord = ResourceType ("Microsoft.Network/dnszones/CNAME", "2018-05-01")
+let txtRecord = ResourceType ("Microsoft.Network/dnszones/TXT", "2018-05-01")
+let mxRecord = ResourceType ("Microsoft.Network/dnszones/MX", "2018-05-01")
+let nsRecord = ResourceType ("Microsoft.Network/dnszones/NS", "2018-05-01")
+let soaRecord = ResourceType ("Microsoft.Network/dnszones/SOA", "2018-05-01")
+let srvRecord = ResourceType ("Microsoft.Network/dnszones/SRV", "2018-05-01")
+let ptrRecord = ResourceType ("Microsoft.Network/dnszones/PTR", "2018-05-01")
 
-type DnsZone =
-    { Name : ResourceName
-      Properties : {| ZoneType : string |} }
-
-    interface IArmResource with
-        member this.ResourceName = this.Name
-        member this.JsonModel =
-            {| ``type`` = zones.ArmValue
-               name = this.Name.Value
-               location = "global"
-               apiVersion = "2018-05-01"
-               properties =
-                   {| zoneType = this.Properties.ZoneType |}
-            |} :> _
-
-module DnsRecords =
-
-    let mapRecordType = function
+type DnsRecordType with
+    member this.ResourceType =
+        match this with
         | Unknown -> failwith "Not Implemented"
         | CName -> cnameRecord
         | A -> aRecord
@@ -45,6 +30,18 @@ module DnsRecords =
         // | Soa -> soaRecord
         // | Srv -> srvRecord
 
+type DnsZone =
+    { Name : ResourceName
+      Properties : {| ZoneType : string |} }
+
+    interface IArmResource with
+        member this.ResourceName = this.Name
+        member this.JsonModel =
+            {| zones.Create(this.Name, Location.Global) with
+                properties = {| zoneType = this.Properties.ZoneType |}
+            |} :> _
+
+module DnsRecords =
     type CNameDnsRecord =
         { Name : ResourceName
           Zone : ResourceName
@@ -55,15 +52,11 @@ module DnsRecords =
         interface IArmResource with
             member this.ResourceName = this.Name
             member this.JsonModel =
-                {| ``type`` = (mapRecordType this.Type).ArmValue
-                   name = this.Zone.Value + "/" + this.Name.Value
-                   apiVersion = "2018-05-01"
-                   properties =
-                        {| TTL = this.TTL;
+                {| this.Type.ResourceType.Create(this.Zone + this.Name, dependsOn = [ this.Zone ]) with
+                    properties =
+                        {| TTL = this.TTL
                            targetResource = {| id = this.TargetResource |}
                            CNAMERecords = {| cname = this.CNameRecord |> Option.toObj |} |}
-                   dependsOn =
-                     [ this.Zone.Value ]
                 |} :> _
 
     type MxDnsRecord =
@@ -75,14 +68,10 @@ module DnsRecords =
         interface IArmResource with
             member this.ResourceName = this.Name
             member this.JsonModel =
-                {| ``type`` = (mapRecordType this.Type).ArmValue
-                   name = this.Zone.Value + "/" + this.Name.Value
-                   apiVersion = "2018-05-01"
-                   properties =
+                {| this.Type.ResourceType.Create(this.Zone + this.Name, dependsOn = [ this.Zone ]) with
+                    properties =
                         {| TTL = this.TTL;
-                           MXRecords = this.MxRecords |> List.map (fun x -> {| preference = x.Preference; exchange = x.Exchange |}) |}
-                   dependsOn =
-                     [ this.Zone.Value ]
+                           MXRecords = this.MxRecords |> List.map (fun mx -> {| preference = mx.Preference; exchange = mx.Exchange |}) |}
                 |} :> _
 
     type NsDnsRecord =
@@ -94,15 +83,11 @@ module DnsRecords =
         interface IArmResource with
             member this.ResourceName = this.Name
             member this.JsonModel =
-                {| ``type`` = (mapRecordType this.Type).ArmValue
-                   name = this.Zone.Value + "/" + this.Name.Value
-                   apiVersion = "2018-05-01"
-                   properties =
-                        {| TTL = this.TTL;
+                {| this.Type.ResourceType.Create(this.Zone + this.Name, dependsOn = [ this.Zone ]) with
+                    properties =
+                        {| TTL = this.TTL
                            targetResource = {| id = null |}
-                           NSRecords = this.NsRecords |> List.map (fun x -> {| nsdname = x |}) |}
-                   dependsOn =
-                     [ this.Zone.Value ]
+                           NSRecords = this.NsRecords |> List.map (fun ns -> {| nsdname = ns |}) |}
                 |} :> _
 
     type TxtDnsRecord =
@@ -114,14 +99,10 @@ module DnsRecords =
         interface IArmResource with
             member this.ResourceName = this.Name
             member this.JsonModel =
-                {| ``type`` = (mapRecordType this.Type).ArmValue
-                   name = this.Zone.Value + "/" + this.Name.Value
-                   apiVersion = "2018-05-01"
-                   properties =
+                {| this.Type.ResourceType.Create(this.Zone + this.Name, dependsOn = [ this.Zone ]) with
+                    properties =
                         {| TTL = this.TTL;
-                           TXTRecords = this.TxtRecords |> List.map (fun x -> {| value = [ x ] |}) |}
-                   dependsOn =
-                     [ this.Zone.Value ]
+                           TXTRecords = this.TxtRecords |> List.map (fun txt -> {| value = [ txt ] |}) |}
                 |} :> _
 
     type PtrDnsRecord =
@@ -133,14 +114,10 @@ module DnsRecords =
         interface IArmResource with
             member this.ResourceName = this.Name
             member this.JsonModel =
-                {| ``type`` = (mapRecordType this.Type).ArmValue
-                   name = this.Zone.Value + "/" + this.Name.Value
-                   apiVersion = "2018-05-01"
-                   properties =
+                {| this.Type.ResourceType.Create(this.Zone + this.Name, dependsOn = [ this.Zone ]) with
+                    properties =
                         {| TTL = this.TTL;
-                           PTRRecords = this.PtrRecords |> List.map (fun x -> {| ptrdname = x |}) |}
-                   dependsOn =
-                     [ this.Zone.Value ]
+                           PTRRecords = this.PtrRecords |> List.map (fun ptr -> {| ptrdname = ptr |}) |}
                 |} :> _
 
     type ADnsRecord =
@@ -153,15 +130,11 @@ module DnsRecords =
         interface IArmResource with
             member this.ResourceName = this.Name
             member this.JsonModel =
-                {| ``type`` = (mapRecordType this.Type).ArmValue
-                   name = this.Zone.Value + "/" + this.Name.Value
-                   apiVersion = "2018-05-01"
-                   properties =
+                {| this.Type.ResourceType.Create(this.Zone + this.Name, dependsOn = [ this.Zone ]) with
+                    properties =
                         {| TTL = this.TTL;
                            targetResource = {| id = this.TargetResource |}
-                           ARecords = this.ARecords |> List.map (fun x -> {| ipv4Address = x |}) |}
-                   dependsOn =
-                     [ this.Zone.Value ]
+                           ARecords = this.ARecords |> List.map (fun a -> {| ipv4Address = a |}) |}
                 |} :> _
 
     type AaaaDnsRecord =
@@ -174,13 +147,9 @@ module DnsRecords =
         interface IArmResource with
             member this.ResourceName = this.Name
             member this.JsonModel =
-                {| ``type`` = (mapRecordType this.Type).ArmValue
-                   name = this.Zone.Value + "/" + this.Name.Value
-                   apiVersion = "2018-05-01"
-                   properties =
+                {| this.Type.ResourceType.Create(this.Zone + this.Name, dependsOn = [ this.Zone ]) with
+                    properties =
                         {| TTL = this.TTL;
                            targetResource = {| id = this.TargetResource |}
-                           AAAARecords = this.AaaaRecords |> List.map (fun x -> {| ipv6Address = x |}) |}
-                   dependsOn =
-                     [ this.Zone.Value ]
+                           AAAARecords = this.AaaaRecords |> List.map (fun aaaa -> {| ipv6Address = aaaa |}) |}
                 |} :> _
