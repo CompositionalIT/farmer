9a01901f36ef2a065ec6e4f3e9b48fdda0dc5753
Isaac Abraham <isaac.abraham@gmail.com>
2020-10-14 14:01:21 +0200

    Improve tests

--- src/Tests/LogAnalytics.fs
+++ src/Tests/LogAnalytics.fs
@@ -5,49 +5,61 @@ open Farmer
 open Farmer.Arm
 open Farmer.Builders
 open Farmer.CoreTypes
+open Farmer.Helpers
 open Farmer.LogAnalytics
+open Microsoft.Azure.Management.OperationalInsights
+open Microsoft.Azure.Management.OperationalInsights.Models
+open Microsoft.Rest
+open System
+
+let dummyClient = new OperationalInsightsManagementClient (Uri "http://management.azure.com", TokenCredentials "NotNullOrWhiteSpace")
+
+let asAzureResource (ws:WorkspaceConfig) =
+    arm { add_resource ws }
+    |> findAzureResources<Workspace> dummyClient.SerializationSettings
+    |> List.head
+    |> fun r ->
+        r.Validate()
+        r
 
 let tests = testList "Log analytics" [
     let makeLogAnalytics theSku =
         logAnalytics {
             name "myFarmer"
             sku theSku
-        } :> IBuilder
+        }
 
     test "Creates a log analytics workspace" {
-        let builder =
+        let config =
             logAnalytics {
                 name "myFarmer"
                 sku (PerGb 30<Days>)
                 enable_query
                 enable_ingestion
-            } :> IBuilder
+            }
+        let ws = asAzureResource config
 
-        let resources = builder.BuildResources Location.WestEurope
-        let workspace = resources.[0] :?> Workspace
-
-        Expect.equal workspace.Location Location.WestEurope "Incorrect Location"
-        Expect.equal workspace.Name (ResourceName "myFarmer") "Incorrect Name"
-        Expect.equal workspace.IngestionSupport (Some Enabled) "Incorrect IngestionSupport"
-        Expect.equal workspace.QuerySupport (Some Enabled) "Incorrect QuerySupport"
-        Expect.equal workspace.Sku (PerGb 30<Days>) "Incorrect Sku"
+        Expect.equal ws.Location "westeurope" "Incorrect Location"
+        Expect.equal ws.Name "myFarmer" "Incorrect Name"
+        Expect.equal ws.PublicNetworkAccessForIngestion "Enabled" "Incorrect IngestionSupport"
+        Expect.equal ws.PublicNetworkAccessForQuery "Enabled" "QuerySupport"
+        Expect.equal ws.Sku.Name "PerGb2018" "Incorrect Sku"
+        Expect.equal ws.RetentionInDays (Nullable 30) "Incorrect Retention In Days"
     }
 
     test "Ingestion and Query are disabled by default" {
-        let builder = makeLogAnalytics (PerGb 30<Days>)
-        let resources = builder.BuildResources Location.WestEurope
-        let workspace = resources.[0] :?> Workspace
+        let ws = makeLogAnalytics (PerGb 30<Days>) |> asAzureResource
 
-        Expect.equal workspace.QuerySupport None "Query should be off by default"
-        Expect.equal workspace.IngestionSupport None "Ingestion should be off by default"
+        Expect.equal ws.PublicNetworkAccessForQuery null "Query should be off by default"
+        Expect.equal ws.PublicNetworkAccessForIngestion null "Ingestion should be off by default"
     }
 
-    test "Can't create log analytics with Sku eqaul to Standalone, PerNode or PerGB2018 with retention period outside 30 and 730 " {
-        Expect.throws (fun _ -> makeLogAnalytics (PerGb 29<Days>) |> ignore) "Should have thrown"
-        Expect.throws (fun _ -> makeLogAnalytics (PerNode 29<Days>) |> ignore) "Should have thrown"
-        Expect.throws (fun _ -> makeLogAnalytics (Standalone 29<Days>) |> ignore) "Should have thrown"
+    test "Can't create log analytics with Sku equal to Standalone, PerNode or PerGB2018 with retention period outside 30 and 730 " {
+        let permutations = List.allPairs [ PerGb; PerNode; Standalone ] [ 29<Days>; 731<Days> ]
+        for (sku, days) in permutations do
+            Expect.throws (fun _ -> makeLogAnalytics (sku days) |> ignore) (sprintf "Should have thrown for %A" (sku days))
     }
-    test "Can't create log analytics with Sku eqaul to Standard and retention_period doesn't equal to 30 " {
+    test "Can't create log analytics with Sku equal to Standard and retention_period doesn't equal to 30 " {
         Expect.throws (fun _ -> makeLogAnalytics (Standalone 29<Days>) |> ignore) "Should have thrown"
     }
 ]
