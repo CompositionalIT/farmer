7526d355100abfd569809edd237104b100705ff8
Luis Quintanilla <lqdev@outlook.com>
2020-11-05 21:05:46 -0500

    Added workspace config properties

--- src/Farmer/Builders/Builders.MachineLearning.fs
+++ src/Farmer/Builders/Builders.MachineLearning.fs
@@ -7,19 +7,53 @@ open Farmer.CoreTypes
 
 // TODO: Implement AML workspace config
 type WorkspaceConfig = 
-    { Name: ResourceName }
+    { WorkspaceName : ResourceName
+      ResourceGroupName : ResourceName
+      Sku : MachineLearning.Sku
+      StorageAccountOption : MachineLearning.StorageAccountOption
+      StorageAccountName : ResourceName
+      StorageAccountType : Storage.Sku
+      StorageAccountBehindVNet : bool
+      StorageAccountResourceName : ResourceName
+      KeyVaultOption : MachineLearning.KeyVaultOption
+      KeyVaultName : ResourceName
+      KeyVaultBehindVNet : bool
+      KeyVaultResourceGroupName : ResourceName
+      ApplicationInsightsOption : MachineLearning.ApplicationInsightsOption
+      ApplicationInsightsName : ResourceName
+      ApplicationInsightsResourceGroupName : ResourceName
+      ContainerRegistryOption : MachineLearning.ContainerRegistryOption
+      ContainerRegistryName : ResourceName
+      ContainerRegistrySku : ContainerRegistry.Sku
+      ContainerRegistryBehindVNet : bool
+      VnetOption : MachineLearning.VnetOption
+      VnetName : ResourceName
+      VnetResourceGroupName : ResourceName
+      AddressPrefixes : IPAddressCidr array
+      SubnetOption : MachineLearning.SubnetOption
+      SubnetName : ResourceName
+      SubnetPrefix : IPAddressCidr
+      AdbWorkspace : ResourceName
+      ConfidentialData : bool
+      EncryptionStatus : MachineLearning.EncryptionStatus
+      CmkKeyVault : MachineLearning.CmkId
+      ResouceCmkUri : MachineLearning.ResourceCmkUri
+      PrivateEndpointType : MachineLearning.PrivateEndpointType
+      PrivateEndpointName : ResourceName
+      PrivateEndpointResourceGroupName : ResourceName
+      Tags : Map<string,string> }
     interface IBuilder with
-        member this.DependencyName = this.Name
+        member this.DependencyName = this.WorkspaceName
         member this.BuildResources location = []
 
 // TODO: Implement AML workspace builder
 type WorkspaceBuilder() = 
-    member _.Yield _ = { Name = ResourceName.Empty }
+    member _.Yield _ = {||}
     member _.Run (state:WorkspaceConfig) = 
         state
     
-    [<CustomOperation "name">]
-    member _.Name(state:WorkspaceConfig) = 
+    [<CustomOperation "workspace_name">]
+    member _.WorkspaceName(state:WorkspaceConfig) = 
         state
 
 let machineLearning = WorkspaceBuilder()
