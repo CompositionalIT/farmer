2097ec9fa34536b7aa3ae9ba933fe68a9222ae4f
Luis Quintanilla <lqdev@outlook.com>
2020-12-16 10:14:08 -0500

    Updating ARM Azure ML

--- src/Farmer/Arm/MachineLearning.fs
+++ src/Farmer/Arm/MachineLearning.fs
@@ -3,45 +3,17 @@ module Farmer.Arm.MachineLearning
 
 open Farmer
 
+//https://docs.microsoft.com/en-us/azure/templates/microsoft.machinelearningservices/2020-08-01/workspaces
+
 let workspaces = ResourceType("Microsoft.MachineLearningServices/workspaces","2020-08-01")
 
 type AzureMachineLearningWorkspace = 
-    { WorkspaceName : ResourceName
-      Location : Location 
-      ResourceGroupName : ResourceName
-      Sku : MachineLearning.Sku
-      StorageAccountOption : MachineLearning.StorageAccountOption
-      StorageAccountName : ResourceName
-      StorageAccountType : Storage.Sku
-      StorageAccountBehindVNet : bool
-      StorageAccountResourceName : ResourceName
-      KeyVaultOption : MachineLearning.KeyVaultOption
-      KeyVaultName : ResourceName
-      KeyVaultBehindVNet : bool
-      KeyVaultResourceGroupName : ResourceName
-      ApplicationInsightsOption : MachineLearning.ApplicationInsightsOption
-      ApplicationInsightsName : ResourceName
-      ApplicationInsightsResourceGroupName : ResourceName
-      ContainerRegistryOption : MachineLearning.ContainerRegistryOption
-      ContainerRegistryName : ResourceName
-      ContainerRegistrySku : ContainerRegistry.Sku
-      ContainerRegistryBehindVNet : bool
-      VnetOption : MachineLearning.VnetOption
-      VnetName : ResourceName
-      VnetResourceGroupName : ResourceName
-      AddressPrefixes : IPAddressCidr array
-      SubnetOption : MachineLearning.SubnetOption
-      SubnetName : ResourceName
-      SubnetPrefix : IPAddressCidr
-      AdbWorkspace : ResourceName
-      ConfidentialData : bool
-      EncryptionStatus : MachineLearning.EncryptionStatus
-      CmkKeyVault : MachineLearning.CmkId
-      ResouceCmkUri : MachineLearning.ResourceCmkUri
-      PrivateEndpointType : MachineLearning.PrivateEndpointType
-      PrivateEndpointName : ResourceName
-      PrivateEndpointResourceGroupName : ResourceName
-      Tags : Map<string,string> }
+    { 
+      Name: string
+      Type: string
+      ApiVersion: string
+      Properties: obj
+    }
     interface IArmResource with
         member this.ResourceId = workspaces.resourceId this.WorkspaceName
         member this.JsonModel = {| |} :> _ // TODO : Create JsonModel
