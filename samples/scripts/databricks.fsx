f2d443c05dee81797cc0c9e80512282098e2f15c
Isaac Abraham <isaac.abraham@gmail.com>
2021-01-31 17:16:36 +0100

    Docs update

--- samples/scripts/databricks.fsx
+++ samples/scripts/databricks.fsx
@@ -9,7 +9,7 @@ let myVault = keyVault { name "my-vault" }
 
 let workspace = databricks {
     name "isaac-databricks"
-    key_vault_managed_keys myVault "workspace-encryption-key"
+    encrypt_with_key_vault myVault "workspace-encryption-key"
     key_vault_key_version Guid.Empty
     attach_to_vnet "databricks-vnet" "databricks-pub-snet" "databricks-priv-snet"
 }
