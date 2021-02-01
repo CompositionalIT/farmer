deeffc16bf05183920349964e44ef1b8f3b6d968
Isaac Abraham <isaac.abraham@gmail.com>
2021-02-02 00:00:26 +0100

    Fairly large refactor

--- src/Farmer/Arm/DiagnosticSetting.fs
+++ src/Farmer/Arm/DiagnosticSetting.fs
@@ -1,83 +1,91 @@
 ﻿[<AutoOpen>]
 module Farmer.Arm.DiagnosticSetting
+
 open System
 open Farmer
 
+let private (|InBounds|OutOfBounds|) days =
+    if days > 365<Days> then OutOfBounds days
+    elif days < 1<Days> then OutOfBounds days
+    else InBounds days
 
-type RetentionPolicy = {
-    Enabled:bool
-    Retention_period:int<Days> }
+type RetentionPolicy =
+    { Enabled : bool
+      RetentionPeriod : int<Days> }
+    static member Create (retentionPeriod, ?enabled) =
+        match retentionPeriod with
+        | OutOfBounds days ->
+            failwithf "The retention period must be between 1 and 365 days. It is currently %d." days
+        | InBounds _ ->
+            { Enabled = defaultArg enabled true
+              RetentionPeriod = retentionPeriod }
 
-type MetricSettings =
-    { Category:string
-      TimeGrain: TimeSpan  option
-      Enabled:bool
-      RetentionPolicy:RetentionPolicy option }
+type MetricSetting =
+    { Category : string
+      TimeGrain : TimeSpan option
+      Enabled : bool
+      RetentionPolicy : RetentionPolicy option }
+    static member Create(category, ?retentionPeriod, ?timeGrain) =
+        { Category = category
+          TimeGrain = timeGrain
+          Enabled = true
+          RetentionPolicy = retentionPeriod |> Option.map(fun days -> RetentionPolicy.Create(days, true)) }
 
-type LogSettings =
-    { Category:string
-      Enabled:bool
-      RetentionPolicy:RetentionPolicy option }
+type LogSetting =
+    { Category : string
+      Enabled : bool
+      RetentionPolicy : RetentionPolicy option }
+    static member Create(category, ?retentionPeriod) =
+        { Category = category
+          Enabled = true
+          RetentionPolicy = retentionPeriod |> Option.map(fun days -> RetentionPolicy.Create(days, true)) }
 
-let private tryGetIso (v:IsoDateTime option) = v |> Option.map(fun v -> v.Value) |> Option.toObj
-
-let diagnosticSettingsType parentResourceType  = 
-    ResourceType(parentResourceType + "providers/diagnosticSettings", "2017-05-01-preview")
-
-let metricSettingsBuilder (state: MetricSettings) =
-    {| category = state.Category
-       enabled = state.Enabled
-       timeGrain = tryGetIso (state.TimeGrain |> Option.map IsoDateTime.OfTimeSpan )
-       retentionPolicy =
-       match state.RetentionPolicy with
-       | None -> null
-       | Some x ->
-           box
-             {| enabled = x.Enabled
-                days = x.Retention_period
-             |}
-    
-    |}
-let logSettingsBuilder (statex:LogSettings) =
-        {| category = statex.Category
-           enabled = statex.Enabled
-           retentionPolicy =
-           match statex.RetentionPolicy with
-           | None -> null
-           | Some x ->
-               box
-                 {| enabled = x.Enabled
-                    days = x.Retention_period
-                 |} 
-        
-        |}
+let diagnosticSettingsType (parent:ResourceType) =
+    ResourceType(parent.Type + "/providers/diagnosticSettings", "2017-05-01-preview")
 
 type DiagnosticSettings =
     { Name : ResourceName
-      ParentResourceType : string
+      ParentResource : ResourceId
       Location : Location
-      Dependencies : ResourceId List
-      StorageAccountId : ResourceId option 
-      ServiceBusRuleId: ResourceId option 
+      StorageAccountId : ResourceId option
+      ServiceBusRuleId: ResourceId option
       EventHubAuthorizationRuleId: ResourceId option
       EventHubName : string option
-      Metrics : MetricSettings array
-      Logs : LogSettings array
+      Metrics : MetricSetting list
+      Logs : LogSetting list
       WorkspaceId : ResourceId option
       DedicatedLogAnalyticsDestination : string option
+      Dependencies : ResourceId Set
       Tags : Map<string, string> }
 
     interface IArmResource with
-        member this.ResourceId = diagnosticSettingsType(this.ParentResourceType).resourceId this.Name
+        member this.ResourceId = diagnosticSettingsType(this.ParentResource.Type).resourceId this.Name
         member this.JsonModel =
-            {| diagnosticSettingsType(this.ParentResourceType).Create(this.Name,this.Location, tags = this.Tags,dependsOn = this.Dependencies) with
+            {| diagnosticSettingsType(this.ParentResource.Type).Create(this.ParentResource.Name/"Microsoft.Insights"/this.Name,this.Location, tags = this.Tags,dependsOn = this.Dependencies) with
                 properties =
                     {| serviceBusRuleId = this.ServiceBusRuleId
-                       LogAnalyticsDestinationType = this.DedicatedLogAnalyticsDestination |> Option.toObj 
+                       LogAnalyticsDestinationType = this.DedicatedLogAnalyticsDestination |> Option.toObj
                        eventHubName = this.EventHubName |> Option.toObj
-                       eventHubAuthorizationRuleId = this.EventHubAuthorizationRuleId |> Option.map(fun x -> x.Eval()) |> Option.toObj 
+                       eventHubAuthorizationRuleId = this.EventHubAuthorizationRuleId |> Option.map(fun x -> x.Eval()) |> Option.toObj
                        storageAccountId = this.StorageAccountId |> Option.map( fun x -> x.Eval()) |> Option.toObj
-                       metrics = this.Metrics |> Array.map  metricSettingsBuilder  
-                       logs = this.Logs |> Array.map  logSettingsBuilder
-                       workspaceId = this.WorkspaceId |> Option.map( fun x -> x.Eval()) |> Option.toObj  |}
+                       metrics = [|
+                           for metric in this.Metrics do
+                            {| category = metric.Category
+                               enabled = metric.Enabled
+                               timeGrain = metric.TimeGrain |> Option.map (IsoDateTime.OfTimeSpan >> fun v -> v.Value) |> Option.toObj
+                               retentionPolicy =
+                                metric.RetentionPolicy
+                                |> Option.map(fun policy -> box {| enabled = policy.Enabled; days = policy.RetentionPeriod |})
+                                |> Option.toObj |}
+                       |]
+                       logs = [|
+                           for log in this.Logs do
+                            {| category = log.Category
+                               enabled = log.Enabled
+                               retentionPolicy =
+                                log.RetentionPolicy
+                                |> Option.map(fun policy -> box {| enabled = policy.Enabled; days = policy.RetentionPeriod |})
+                                |> Option.toObj |}
+                       |]
+                       workspaceId = this.WorkspaceId |> Option.map( fun x -> x.Eval()) |> Option.toObj |}
             |} :> _
