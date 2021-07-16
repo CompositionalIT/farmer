fb7a9912424c450014a37ec079f7b2c26ac40d1a
aagill <aagill@microsoft.com>
2021-07-16 16:23:39 -0400

    squashing

--- src/Farmer/Arm/Network.fs
+++ src/Farmer/Arm/Network.fs
@@ -241,6 +241,7 @@ type NetworkInterface =
         {| SubnetName : ResourceName
            PublicIpAddress : LinkedResource option |} list
       VirtualNetwork : ResourceName
+      PrivateIpAllocation: PrivateIpAddress.AllocationMethod option
       Tags: Map<string,string>  }
     interface IArmResource with
         member this.ResourceId = networkInterfaces.resourceId this.Name
@@ -260,7 +261,12 @@ type NetworkInterface =
                         |> List.mapi(fun index ipConfig ->
                             {| name = $"ipconfig{index + 1}"
                                properties =
-                                {| privateIPAllocationMethod = "Dynamic"
+                                let allocationMethod, ip = 
+                                    match this.PrivateIpAllocation with 
+                                    | Some (StaticPrivateIp ip) -> "Static", string ip
+                                    | _ -> ("Dynamic", null)
+                                {| privateIPAllocationMethod = allocationMethod
+                                   privateIPAddress = ip
                                    publicIPAddress = 
                                        ipConfig.PublicIpAddress 
                                        |> Option.map(fun pip -> {| id = pip.ResourceId.ArmExpression.Eval() |}) 
