aea530a502e5402b6a6d140850abc35387a54769
Dave Curylo <curylod@asme.org>
2021-08-19 22:56:22 -0400

    ACI fix to generate parameter for initContainer env vars

--- src/Tests/ContainerGroup.fs
+++ src/Tests/ContainerGroup.fs
@@ -300,6 +300,25 @@ let tests = testList "Container Group" [
         Expect.hasLength deployment.Template.Parameters 1 "Should have a secure parameter for environment variable"
         Expect.equal (deployment.Template.Parameters.Head.ArmExpression.Eval()) "[parameters('secret-foo')]" "Generated incorrect secure parameter."
     }
+    test "Secure environment variables are generated for init containers" {
+        let cg = containerGroup {
+            name "myapp"
+            add_init_containers [
+                initContainer {
+                    name "nginx"
+                    image "nginx:1.17.6-alpine"
+                    env_vars [
+                        EnvVar.createSecure "foo" "secret-init"
+                    ]
+                }
+            ]
+        }
+        let deployment = arm {
+            add_resource cg
+        }
+        Expect.hasLength deployment.Template.Parameters 1 "Should have a secure parameter for initContainer environment variable"
+        Expect.equal (deployment.Template.Parameters.Head.ArmExpression.Eval()) "[parameters('secret-init')]" "Generated incorrect secure parameter."
+    }
     test "Secure parameters for secret volume is generated correctly" {
         let cg = containerGroup {
             name "myapp"
