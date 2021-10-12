e0693cdb1167f1f382c540827136b339c70b177f
Dave Curylo <david.curylo@microsoft.com>
2021-10-12 10:05:07 -0400

    Adds skeleton builder for test DSL

--- src/Tests/AppGateway.fs
+++ src/Tests/AppGateway.fs
@@ -2,8 +2,29 @@ module AppGateway
 
 open Expecto
 
+module Builders =
+    type AppGatewayConfig = {
+        Name: string
+    }
+
+    type AppGatewayBuilder () =
+        member _.Yield _ : AppGatewayConfig = {
+            Name = ""
+        }
+        [<CustomOperation "name">]
+        member _.Name (config:AppGatewayConfig, name:string) =
+            { config with Name = name }
+
+    let appGateway = AppGatewayBuilder ()
+
+open Builders
+
 let tests = testList "Application Gateway Tests" [
     ftest "No AppGateway yet" {
+        let ag =
+            appGateway {
+                name "appGateway"
+            }
         ()
     }
 ]
