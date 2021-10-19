0d4133d4ca0520f59e8f6e87654972a0e8e289fd
Dave Curylo <curylod@asme.org>
2021-10-19 00:14:36 -0400

    Full sample with ACI backend

--- samples/scripts/applicationGateway.fsx
+++ samples/scripts/applicationGateway.fsx
@@ -4,6 +4,7 @@ open Farmer
 
 open Farmer.NetworkSecurity
 open Farmer.Identity
+open Farmer.Network
 open Farmer.Builders
 open Farmer.ApplicationGateway
 open Farmer.Arm.Network
@@ -51,6 +52,9 @@ let net = vnet {
                     name "apps"
                     size 24
                     network_security_group myNsg
+                    add_delegations [
+                        SubnetDelegationService.ContainerGroups
+                    ]
                 }
                 subnetSpec {
                     name "no-nsg"
@@ -65,71 +69,72 @@ let msi = createUserAssignedIdentity "agw-msi"
 let backendPoolName = ResourceName "agw-be-pool"
 
 let myAppGateway =
+    let gwIp =
+        gatewayIp {
+            name "app-gw-ip"
+            link_to_subnet net.Name net.Subnets.[0].Name
+        }
+    let frontendIp =
+        frontendIp {
+            name "app-gw-fe-ip"
+            public_ip "agp-gw-pip"
+        }
+    let frontendPort =
+        frontendPort {
+            name "port-80"
+            port 80
+        }
+    let listener =
+        httpListener {
+            name "http-listener"
+            frontend_ip frontendIp
+            frontend_port frontendPort
+            backend_pool backendPoolName.Value
+        }
+    let backendPool =
+        appGatewayBackendAddressPool {
+            name backendPoolName.Value
+            add_backend_addresses [
+                backend_ip_address "10.28.1.4"
+                backend_ip_address "10.28.1.5"
+            ]
+        }
+    let healthProbe =
+        appGatewayProbe {
+            name "agw-probe"
+            host "localhost"
+            path "/"
+            port 80
+            protocol Protocol.Http
+        }
+    let backendSettings =
+        backendHttpSettings {
+            name "bp-default-web-80-web-80"
+            port 80
+            probe healthProbe
+            protocol Protocol.Http
+            request_timeout 10<Seconds>
+        }
+    let routingRule =
+        basicRequestRoutingRule {
+            name "web-front-to-aci-back"
+            http_listener listener
+            backend_address_pool backendPool
+            backend_http_settings backendSettings
+        }
+    
     appGateway {
-        name "agw-bldr-test"
+        name "app-gw"
         sku_capacity 2
         add_identity msi
-        add_ip_configs [
-            gatewayIp {
-                name "agw-gwip"
-                link_to_subnet net.Name net.Subnets.[0].Name
-            }
-        ]
-        add_frontends [
-            frontendIp {
-                name "frontend-ip"
-                public_ip "agw-bldr-pip"
-            }
-        ]
-        add_frontend_ports [
-            frontendPort {
-                name "port-80"
-                port 80
-            }
-        ]
-        add_http_listeners [
-            httpListener {
-                name "http-listener"
-                frontend_ip "frontend-ip"
-                frontend_port "port-80"
-                backend_pool backendPoolName.Value
-            }
-        ]
-        add_backend_address_pools [
-            appGatewayBackendAddressPool {
-                name backendPoolName.Value
-                add_backend_addresses [
-                    backend_ip_address "10.28.1.4"
-                    backend_ip_address "10.28.1.5"
-                ]
-            }
-        ]
-        add_backend_http_settings_collection [
-            backendHttpSettings {
-                name "bp-default-web-80-9090-web"
-                port 9090us
-                probe "agw-probe"
-                protocol Protocol.Http
-                request_timeout 30<Seconds>
-            }
-        ]
-        add_request_routing_rules [
-            basicRequestRoutingRule {
-                name "rr"
-                http_listener "http-listener"
-                backend_address_pool backendPoolName.Value
-                backend_http_settings "bp-default-web-80-9090-web"
-            }
-        ]
-        add_probes [
-            appGatewayProbe {
-                name "agw-probe"
-                host "localhost"
-                path "/"
-                port 8080
-                protocol Protocol.Http
-            }
-        ]
+        add_ip_configs [ gwIp ]
+        add_frontends [ frontendIp ]
+        add_frontend_ports [ frontendPort ]
+        add_http_listeners [ listener ]
+        add_backend_address_pools [ backendPool ]
+        add_backend_http_settings_collection [ backendSettings ]
+        add_request_routing_rules [ routingRule ]
+        add_probes [ healthProbe ]
         depends_on myNsg
         depends_on net
    }
@@ -141,6 +146,33 @@ arm {
         net
         myNsg
         myAppGateway
+        networkProfile {
+            name "hello-aci-netprofile"
+            vnet net.Name.Value
+            subnet net.Subnets.[1].Name.Value
+        }
+        containerGroup {
+            name "hello-aci1"
+            add_instances [
+                containerInstance {
+                    name "hello"
+                    image "mcr.microsoft.com/azuredocs/aci-helloworld"
+                    add_internal_ports [ 80us ]
+                }
+            ]
+            network_profile "hello-aci-netprofile"
+        }
+        containerGroup {
+            name "hello-aci2"
+            add_instances [
+                containerInstance {
+                    name "hello"
+                    image "mcr.microsoft.com/azuredocs/aci-helloworld"
+                    add_internal_ports [ 80us ]
+                }
+            ]
+            network_profile "hello-aci-netprofile"
+        }
     ]
 } |> Writer.quickWrite "AGW-bldr"
 
