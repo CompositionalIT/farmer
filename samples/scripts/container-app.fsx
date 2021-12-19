ed0098d51cd4ac5aec80f710e47334e4f43c8daf
Isaac Abraham <isaac.abraham@gmail.com>
2021-12-19 16:11:24 +0100

    Script cleanup

--- samples/scripts/container-app.fsx
+++ samples/scripts/container-app.fsx
@@ -6,7 +6,7 @@ open Farmer.ContainerApp
 open System
 
 let queueName = "myqueue"
-let storageName = "isaacsuperstore"
+let storageName = $"{Guid.NewGuid().ToString().[0..5]}containerqueue"
 let myStorageAccount = storageAccount {
     name storageName
     add_queue queueName
@@ -19,7 +19,6 @@ let env =
             containerApp {
                 name "aspnetsample"
                 add_simple_container "mcr.microsoft.com/dotnet/samples" "aspnetapp"
-                ingress_visibility External
                 ingress_target_port 80us
                 ingress_transport Auto
                 add_http_scale_rule "http-scaler" { ConcurrentRequests = 10 }
@@ -50,41 +49,4 @@ let template =
     }
 
 template
-|> Writer.quickWrite "sample"
-
-
-
-template
-|> Deploy.execute "isaaccontainertest" []
-
-//active_revision_mode ActiveRevisionsMode.Single
-//replicas 1 5
-//add_env_variable "ServiceBusQueueName" "wishrequests"
-//add_app_secret "ServiceBusConnectionKey"
-//dapr_app_id "http"
-//add_scale_rule "http-rule" (ScaleRule.Http { ConcurrentRequests = 100 })
-
-//containerApp {
-//    name "servicebus"
-//    active_revision_mode ActiveRevisionsMode.Single
-//    docker_image containerRegistryDomain containerRegistry "servicebus" version
-//    replicas 0 3
-//    add_env_variable "ServiceBusQueueName" "wishrequests"
-//    add_app_secret "ServiceBusConnectionKey"
-//    add_scale_rule
-//        "sb-keda-scale"
-//        (ScaleRule.ServiceBus {
-//            QueueName = "wishrequests"
-//            MessageCount = 5
-//            SecretRef = "servicebusconnectionkey" })
-//}
-
-
-#r "nuget:Azure.Storage.Queues"
-
-open Azure.Storage.Queues
-
-let queue = Azure.Storage.Queues.QueueClient("DefaultEndpointsProtocol=https;AccountName=isaacsuperstore;AccountKey=WreOm4w+5uI//c+3Xa1GWIc/Pw2g6guF56f/lCik259yOAP7ymlHPggMS4kODge05EleuiQpI48AHzUhbPU7DQ==;EndpointSuffix=core.windows.net", "myqueue", QueueClientOptions(MessageEncoding = QueueMessageEncoding.None))
-
-for i in 1 .. 10000 do
-    queue.SendMessageAsync $"test-{i}" |> ignore
+|> Deploy.execute "containerappsdemo" []
