197adb42f80c5fd7ca3e723eaf7ef66c8390eee8
Richard Sanderson-Pope <r.sandersonpope@codat.io>
2022-03-14 10:17:01 +0000

    code review

--- src/Farmer/Arm/Web.fs
+++ src/Farmer/Arm/Web.fs
@@ -9,7 +9,7 @@ open System
 
 let serverFarms = ResourceType ("Microsoft.Web/serverfarms", "2018-02-01")
 let sites = ResourceType ("Microsoft.Web/sites", "2021-03-01")
-let config = ResourceType ("Microsoft.Web/sites/config", "2016-08-01")
+let config = ResourceType ("Microsoft.Web/sites/config", "2020-06-01")
 let sourceControls = ResourceType ("Microsoft.Web/sites/sourcecontrols", "2019-08-01")
 let staticSites = ResourceType ("Microsoft.Web/staticSites", "2019-12-01-preview")
 let siteExtensions = ResourceType ("Microsoft.Web/sites/siteextensions", "2020-06-01")
