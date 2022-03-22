5f3ada69bd94dbf2c7afc441eacd3cf94137abff
Richard Sanderson-Pope <r.sandersonpope@codat.io>
2022-03-22 12:32:24 +0000

    Fix test failure due to increased validation

--- src/Tests/Functions.fs
+++ src/Tests/Functions.fs
@@ -416,7 +416,8 @@ let tests = testList "Functions tests" [
     }
     test "Can integrate unmanaged vnet" {
         let subnetId = Arm.Network.subnets.resourceId (ResourceName "my-vnet", ResourceName "my-subnet") 
-        let wa = functions { name "testApp"; link_to_unmanaged_vnet subnetId }
+        let asp = serverFarms.resourceId "my-asp"
+        let wa = functions { name "testApp"; link_to_unmanaged_service_plan asp; link_to_unmanaged_vnet subnetId }
         
         let resources = wa |> getResources
         let site = resources |> getResource<Web.Site> |> List.head
@@ -428,7 +429,8 @@ let tests = testList "Functions tests" [
     }    
     test "Can integrate managed vnet" {
         let vnetConfig = vnet { name "my-vnet" } 
-        let wa = functions { name "testApp"; link_to_vnet (vnetConfig, ResourceName "my-subnet") }
+        let asp = serverFarms.resourceId "my-asp"
+        let wa = functions { name "testApp"; link_to_unmanaged_service_plan asp;  link_to_vnet (vnetConfig, ResourceName "my-subnet") }
             
         let resources = wa |> getResources
         let site = resources |> getResource<Web.Site> |> List.head
