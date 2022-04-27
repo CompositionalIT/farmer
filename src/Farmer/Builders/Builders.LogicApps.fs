9446f92229ebdd936e5e93e4ee6f2a2a210cc370
Max Paige <max.paige@binarydefense.com>
2022-04-27 13:19:10 -0400

    Very basic support for logic apps

---
+++ src/Farmer/Builders/Builders.LogicApps.fs
@@ -0,0 +1,53 @@
+[<AutoOpen>]
+module Farmer.Builders.LogicApps
+
+open Farmer
+open Farmer.Arm.LogicApps
+open System.IO
+open System.Text.Json
+
+type Definition =
+| FileDefinition of path: string
+| ValueDefinition of definition: string
+
+type LogicAppConfig =
+    {
+        WorkflowName: ResourceName
+        Definition: Definition
+        Tags: Map<string, string>
+    }
+
+    member this.LogicAppWorkflowName = workflows.resourceId(this.WorkflowName).Name
+
+    interface IBuilder with
+        member this.ResourceId = workflows.resourceId this.WorkflowName
+        member this.BuildResources location = [
+            {
+                Name = this.LogicAppWorkflowName
+                Location = location
+                Definition =
+                  match this.Definition with
+                  | FileDefinition path ->
+                    let fileContent = File.ReadAllText(path)
+                    JsonDocument.Parse(fileContent)
+                  | ValueDefinition value -> JsonDocument.Parse(value)
+                Tags = this.Tags
+            }
+        ]
+
+type LogicAppBuilder() =
+    member _.Yield _ =
+        {
+            WorkflowName = ResourceName "logic-app-workflow"
+            Definition = ValueDefinition """{"name":"logic-app-workflow"}"""
+            Tags = Map.empty
+        }
+    [<CustomOperation "name">]
+    member _.Name(state: LogicAppConfig, name) = { state with WorkflowName = ResourceName name }
+
+    [<CustomOperation "definition">]
+    member _.Definition(state: LogicAppConfig, definition: Definition) = { state with Definition = definition }
+
+    interface ITaggable<LogicAppConfig> with member _.Add state tags = { state with Tags = state.Tags |> Map.merge tags }
+
+let logicApp = LogicAppBuilder()
---
+++ src/Tests/LogicApps.fs
@@ -0,0 +1,70 @@
+module LogicApps
+
+open Expecto
+open Farmer
+open Farmer.Arm
+open Farmer.Builders
+open Farmer.Helpers
+open Microsoft.Azure.Management.Logic
+open Microsoft.Azure.Management.Logic.Models
+open Microsoft.Rest
+open System
+open System.Text.Json
+
+let dummyClient = new LogicManagementClient(Uri "http://management.azure.com", TokenCredentials "NotNullOrWhiteSpace")
+
+let asAzureResource (lac: LogicAppConfig) =
+  arm { add_resource lac }
+  |> findAzureResources<Workflow> dummyClient.SerializationSettings
+  |> List.head
+  |> fun r ->
+      r.Validate()
+      r
+
+let tests = ftestList "Logic Apps" [
+    test "Creates a logic app workflow" {
+        let config =
+            logicApp {
+                name "test-logic-app"
+            }
+        let workflow = asAzureResource config
+
+        Expect.equal workflow.Name "test-logic-app" "Incorrect workflow name"
+    }
+    test "Populates a value-based logic app definition" {
+        // this is the required bare minimum for an empty logic app
+        // for it to not be set to "null" after parsing
+        let value =
+            """
+            {
+                "definition": {
+                    "$schema": "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
+                    "actions": {},
+                    "contentVersion": "1.0.0.0",
+                    "outputs": {},
+                    "parameters": {},
+                    "triggers": {}
+                },
+                "parameters": {}
+            }
+            """
+        let config = 
+            logicApp {
+                name "test-logic-app"
+                definition (ValueDefinition value)
+            }
+        let workflow = asAzureResource config
+
+        Expect.isNotNull workflow.Definition "Did not set logic app definition"
+    }
+    test "Populates a file-based logic app definition" {
+        let path = "./test-data/blank-logic-app.json"
+        let config = 
+            logicApp {
+                name "test-logic-app"
+                definition (FileDefinition path)
+            }
+        let workflow = asAzureResource config
+        Expect.isNotNull workflow.Definition "Did not load definition from file"
+    }
+]
