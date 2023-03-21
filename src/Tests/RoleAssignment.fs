6afe7e45d807ba04438d133ff0e18349e0fab485
Eugene Tolmachev <eugene.tolmachev@microsoft.com>
2023-03-21 16:07:28 -0400

    formatting

--- src/Tests/RoleAssignment.fs
+++ src/Tests/RoleAssignment.fs
@@ -9,15 +9,19 @@ let tests =
         "RoleAssignment"
         [
             test "Produces opaque resource scope" {
-                let actual : IArmResource = 
-                    { Name = ResourceName "assignment"
-                      RoleDefinitionId = Roles.Contributor
-                      PrincipalId = ArmExpression.create "1" |> PrincipalId
-                      PrincipalType = PrincipalType.User
-                      Scope = privateClouds.resourceId "mySDDC" |> UnmanagedResource 
-                      Dependencies = Set.empty }
+                let actual: IArmResource =
+                    {
+                        Name = ResourceName "assignment"
+                        RoleDefinitionId = Roles.Contributor
+                        PrincipalId = ArmExpression.create "1" |> PrincipalId
+                        PrincipalType = PrincipalType.User
+                        Scope = privateClouds.resourceId "mySDDC" |> UnmanagedResource
+                        Dependencies = Set.empty
+                    }
 
                 "Expected matching scope"
-                |> Expect.stringContains (Newtonsoft.Json.JsonConvert.SerializeObject actual.JsonModel) "\"scope\":\"[resourceId('Microsoft.AVS/privateClouds', 'mySDDC')]\""
+                |> Expect.stringContains
+                    (Newtonsoft.Json.JsonConvert.SerializeObject actual.JsonModel)
+                    "\"scope\":\"[resourceId('Microsoft.AVS/privateClouds', 'mySDDC')]\""
             }
         ]
