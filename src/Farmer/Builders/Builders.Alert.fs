e2e4d40d2a4def75d752cea5b2236bfbdf50ecd7
Peter Dubec <peter@compositional-it.com>
2023-07-20 00:14:33 +0100

    update resource: alerts can now be linked to action groups

--- src/Farmer/Builders/Builders.Alert.fs
+++ src/Farmer/Builders/Builders.Alert.fs
@@ -2,8 +2,8 @@
 module Farmer.Builders.Alert
 
 open Farmer
-open Farmer.Insights
 open Farmer.Arm.InsightsAlerts
+open System
 
 type AlertConfig =
     {
@@ -39,45 +39,45 @@ type AlertBuilder() =
     member __.Yield _ =
         {
             Name = ResourceName.Empty
-            Description = ""
+            Description = String.Empty
             Severity = AlertSeverity.Error
             Frequency = System.TimeSpan(0, 5, 0) |> IsoDateTime.OfTimeSpan
             Window = System.TimeSpan(0, 15, 0) |> IsoDateTime.OfTimeSpan
             Actions = List.empty
             LinkedResources = List.empty
-            Criteria = SingleResourceMultipleMetricCriteria []
+            Criteria = SingleResourceMultipleMetricCriteria List.empty
         }
 
-    [<CustomOperation "name">]
     /// Sets the name of the alert.
+    [<CustomOperation "name">]
     member __.Name(state: AlertConfig, name) = { state with Name = ResourceName name }
 
-    [<CustomOperation "description">]
     /// Sets the description of the alert.
+    [<CustomOperation "description">]
     member __.Description(state: AlertConfig, description) =
         { state with Description = description }
 
-    [<CustomOperation "frequency">]
     /// How often the metric alert is evaluated
+    [<CustomOperation "frequency">]
     member __.Frequency(state: AlertConfig, frequency) = { state with Frequency = frequency }
 
-    [<CustomOperation "window">]
     /// The period of time that is used to monitor alert activity based on the threshold.
+    [<CustomOperation "window">]
     member __.Window(state: AlertConfig, window) = { state with Window = window }
 
-    [<CustomOperation "severity">]
     /// Alert severity
+    [<CustomOperation "severity">]
     member __.Severity(state: AlertConfig, severity) = { state with Severity = severity }
 
-    [<CustomOperation "add_linked_resources">]
     /// Add the target resources on which the alert is created/updated.
+    [<CustomOperation "add_linked_resources">]
     member __.LinkedResources(state: AlertConfig, linked_resources) =
         { state with
             LinkedResources = linked_resources @ state.LinkedResources
         }
 
-    [<CustomOperation "add_linked_resource">]
     /// Add target resource on which the alert is created/updated.
+    [<CustomOperation "add_linked_resource">]
     member this.LinkedResource(state: AlertConfig, linked_resource) =
         { state with
             LinkedResources = linked_resource :: state.LinkedResources
@@ -89,41 +89,44 @@ type AlertBuilder() =
     member this.LinkedResource(state: AlertConfig, builder: IBuilder) =
         this.LinkedResource(state, builder.ResourceId |> Managed)
 
-    [<CustomOperation "single_resource_multiple_metric_criteria">]
     /// The rule criteria that defines the conditions of the alert rule.
+    [<CustomOperation "single_resource_multiple_metric_criteria">]
     member __.SingleCriteria(state: AlertConfig, criteria) =
         { state with
             Criteria = SingleResourceMultipleMetricCriteria criteria
         }
 
-    [<CustomOperation "single_resource_multiple_custom_metric_criteria">]
     /// The rule criteria that defines the conditions of the alert rule based on Application Insights custom metric.
+    [<CustomOperation "single_resource_multiple_custom_metric_criteria">]
     member __.SingleCustomMetricCriteria(state: AlertConfig, criteria) =
         { state with
             Criteria = SingleResourceMultipleCustomMetricCriteria criteria
         }
 
-    [<CustomOperation "multiple_resource_multiple_metric_criteria">]
     /// The rule criterias that defines the conditions of the alert rule.
+    [<CustomOperation "multiple_resource_multiple_metric_criteria">]
     member __.MultiCriteria(state: AlertConfig, criteria) =
         { state with
             Criteria = MultipleResourceMultipleMetricCriteria criteria
         }
 
-    [<CustomOperation "webtest_location_availability_criteria">]
     /// The rule criteria that defines the conditions of the alert rule.
     /// AppInsightsId * WebTestId * FailedLocationCount
     /// If webtest is failing at the same time from x different locations
+    [<CustomOperation "webtest_location_availability_criteria">]
     member __.WebCriteria(state: AlertConfig, criteria) =
         { state with
             Criteria = WebtestLocationAvailabilityCriteria criteria
         }
 
-    [<CustomOperation "add_action">]
     /// Add an action that are performed when the alert rule becomes active.
-    member __.Actions(state: AlertConfig, action) =
+    [<CustomOperation "add_action">]
+    member this.Actions(state: AlertConfig, actionGroupId, ?webHookProperties: obj) =
         { state with
-            Actions = action :: state.Actions
+            Actions = { actionGroupId = actionGroupId; webHookProperties = webHookProperties } :: state.Actions
         }
+    member this.Actions(state: AlertConfig, ag: ActionGroupConfig, ?webHookProperties: obj) =
+        this.Actions(state, ag.ActionGroupId, webHookProperties)
+
 
 let alert = AlertBuilder()
