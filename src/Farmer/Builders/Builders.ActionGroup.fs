f2b8c897aa31cd30b49309c23ab18677ff020998
Peter Dubec <peter@compositional-it.com>
2023-08-23 02:18:28 +0100

    Change __. to _.

--- src/Farmer/Builders/Builders.ActionGroup.fs
+++ src/Farmer/Builders/Builders.ActionGroup.fs
@@ -50,7 +50,7 @@ type ActionGroupConfig =
             [ actionGroup ]
 
 type ActionGroupBuilder() =
-    member __.Yield _ =
+    member _.Yield _ =
         {
             Name = ResourceName.Empty
             GroupShortName = ResourceName.Empty
@@ -70,11 +70,11 @@ type ActionGroupBuilder() =
 
     /// Sets the name of the action group.
     [<CustomOperation "name">]
-    member __.Name(state: ActionGroupConfig, name) = { state with Name = ResourceName name }
+    member _.Name(state: ActionGroupConfig, name) = { state with Name = ResourceName name }
 
     /// Sets the short name of the action group.
     [<CustomOperation "short_name">]
-    member __.GroupShortName(state: ActionGroupConfig, shortName) =
+    member _.GroupShortName(state: ActionGroupConfig, shortName) =
         { state with
             GroupShortName = ResourceName shortName
         }
@@ -85,14 +85,14 @@ type ActionGroupBuilder() =
 
     /// Add ARM Role Receivers.
     [<CustomOperation "add_arm_role_receivers">]
-    member __.ArmRoleReceivers(state: ActionGroupConfig, armRoleReceivers) =
+    member _.ArmRoleReceivers(state: ActionGroupConfig, armRoleReceivers) =
         { state with
             ArmRoleReceivers = (armRoleReceivers @ state.ArmRoleReceivers)
         }
 
     /// Add Automation Runbook Receivers.
     [<CustomOperation "add_automation_runbook_receivers">]
-    member __.AutomationRunbookReceivers
+    member _.AutomationRunbookReceivers
         (
             state: ActionGroupConfig,
             automationRunbookReceivers: AutomationRunbookReceiver list
@@ -103,63 +103,63 @@ type ActionGroupBuilder() =
 
     /// Add Azure App Push Receivers.
     [<CustomOperation "add_azure_app_push_receivers">]
-    member __.AzureAppPushReceivers(state: ActionGroupConfig, azureAppPushReceivers) =
+    member _.AzureAppPushReceivers(state: ActionGroupConfig, azureAppPushReceivers) =
         { state with
             AzureAppPushReceivers = (azureAppPushReceivers @ state.AzureAppPushReceivers)
         }
 
     /// Add Azure Function Receivers.
     [<CustomOperation "add_azure_function_receivers">]
-    member __.AzureFunctionReceivers(state: ActionGroupConfig, azureFunctionReceivers) =
+    member _.AzureFunctionReceivers(state: ActionGroupConfig, azureFunctionReceivers) =
         { state with
             AzureFunctionReceivers = (azureFunctionReceivers @ state.AzureFunctionReceivers)
         }
 
     /// Add Event Hub Receivers.
     [<CustomOperation "add_event_hub_receivers">]
-    member __.EventHubReceivers(state: ActionGroupConfig, eventHubReceivers) =
+    member _.EventHubReceivers(state: ActionGroupConfig, eventHubReceivers) =
         { state with
             EventHubReceivers = (eventHubReceivers @ state.EventHubReceivers)
         }
 
     /// Add ITSM Receivers.
     [<CustomOperation "add_itsm_receivers">]
-    member __.ItsmReceivers(state: ActionGroupConfig, itsmReceivers) =
+    member _.ItsmReceivers(state: ActionGroupConfig, itsmReceivers) =
         { state with
             ItsmReceivers = (itsmReceivers @ state.ItsmReceivers)
         }
 
     /// Add Logic App Receivers.
     [<CustomOperation "add_logic_app_receivers">]
-    member __.LogicAppReceivers(state: ActionGroupConfig, logicAppReceivers) =
+    member _.LogicAppReceivers(state: ActionGroupConfig, logicAppReceivers) =
         { state with
             LogicAppReceivers = (logicAppReceivers @ state.LogicAppReceivers)
         }
 
     /// Add Voice Receivers.
     [<CustomOperation "add_voice_receivers">]
-    member __.VoiceReceivers(state: ActionGroupConfig, voiceReceivers) =
+    member _.VoiceReceivers(state: ActionGroupConfig, voiceReceivers) =
         { state with
             VoiceReceivers = (voiceReceivers @ state.VoiceReceivers)
         }
 
     /// Add SMS Receivers.
     [<CustomOperation "add_sms_receivers">]
-    member __.SMSReceivers(state: ActionGroupConfig, smsReceivers) =
+    member _.SMSReceivers(state: ActionGroupConfig, smsReceivers) =
         { state with
             SMSReceivers = (smsReceivers @ state.SMSReceivers)
         }
 
     /// Add Email Receivers.
     [<CustomOperation "add_email_receivers">]
-    member __.EmailReceivers(state: ActionGroupConfig, emailReceivers) =
+    member _.EmailReceivers(state: ActionGroupConfig, emailReceivers) =
         { state with
             EmailReceivers = (emailReceivers @ state.EmailReceivers)
         }
 
     /// Add Webhook Receivers.
     [<CustomOperation "add_webhook_receivers">]
-    member __.WebhookReceivers(state: ActionGroupConfig, webhookReceivers) =
+    member _.WebhookReceivers(state: ActionGroupConfig, webhookReceivers) =
         { state with
             WebhookReceivers = (webhookReceivers @ state.WebhookReceivers)
         }
