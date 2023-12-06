400792654ec8c4c3502f6efc51a7a1513beec184
Dave Curylo <dave@curylo.org>
2023-12-06 15:38:29 -0500

    Validate structure of entire generated ARM resource in test

--- src/Tests/AutoscaleSettings.fs
+++ src/Tests/AutoscaleSettings.fs
@@ -8,7 +8,7 @@ open Newtonsoft.Json.Linq
 
 let tests =
     testList "Autoscale Settings" [
-        test "Basic Autoscale Settings ARM Resource (no builder)" {
+        ftest "Basic Autoscale Settings ARM Resource (no builder)" {
             let settings =
                 {
                     Name = ResourceName "test-autoscale-settings"
@@ -85,14 +85,14 @@ let tests =
                 add_resources [
                     vmss {
                         name "my-vmss"
-                        add_availability_zones [ "1"; "2" ]
                         vm_profile (
                             vm {
-                                vm_size Vm.Standard_B1ms
+                                vm_size Vm.Standard_B1s
                                 username "azureuser"
                                 operating_system Vm.UbuntuServer_2204LTS
                                 os_disk 30 Vm.Premium_LRS
                                 no_data_disk
+                                custom_script "apt update && apt install -y stress"
                             }
                         )
                     }
@@ -109,5 +109,93 @@ let tests =
             Expect.isNotNull
                 autoscaleProps["name"]
                 "Autoscale properties name is null"
+            Expect.equal
+                (string autoscaleProps["name"])
+                "test-autoscale-settings"
+                "'properties.name' should equal the 'name' of the autoscale setting"
+            Expect.isTrue
+                (autoscaleProps["enabled"].ToObject<bool>())
+                "properties.enabled should be true"
+            Expect.equal
+                (autoscaleProps["targetResourceUri"].ToObject<string>())
+                "[resourceId('Microsoft.Compute/virtualMachineScaleSets', 'my-vmss')]"
+                "Incorrect properties.targetResourceUri"
+            Expect.hasLength
+                (autoscaleProps.SelectToken "profiles")
+                1
+                "Should have one autoscale profile."
+            let profile = autoscaleProps.SelectToken "profiles[0]"
+            Expect.equal
+                (profile.SelectToken "capacity.default" |> string)
+                "1"
+                "Incorrect profile[0].capacity.default"
+            Expect.equal
+                (profile.SelectToken "capacity.maximum" |> string)
+                "10"
+                "Incorrect profile[0].capacity.maximum"
+            Expect.equal
+                (profile.SelectToken "capacity.minimum" |> string)
+                "1"
+                "Incorrect profile[0].capacity.minimum"
+            Expect.equal
+                (profile.SelectToken "name" |> string)
+                "DefaultAutoscaleProfile"
+                "Incorrect name for autoscale profile"
+            Expect.hasLength
+                (profile.SelectToken "rules")
+                2
+                "Should have two rules in autoscale profile."
+            let rule1 = profile.SelectToken "rules[0]"
+            Expect.isFalse
+                ((rule1.SelectToken "metricTrigger.dividePerInstance").ToObject<bool>())
+                "First rule metric trigger should not divide per instance"
+            Expect.equal
+                ((rule1.SelectToken "metricTrigger.metricName").ToObject<string>())
+                "Percentage CPU"
+                "First rule metric trigger metricName incorrect"
+            Expect.equal
+                ((rule1.SelectToken "metricTrigger.metricResourceUri").ToObject<string>())
+                "[resourceId('Microsoft.Compute/virtualMachineScaleSets', 'my-vmss')]"
+                "First rule metric trigger metricResourceUri incorrect"
+            Expect.equal
+                ((rule1.SelectToken "metricTrigger.operator").ToObject<string>())
+                "GreaterThan"
+                "First rule metric trigger operator incorrect"
+            Expect.equal
+                ((rule1.SelectToken "metricTrigger.statistic").ToObject<string>())
+                "Average"
+                "First rule metric trigger statistic incorrect"
+            Expect.equal
+                ((rule1.SelectToken "metricTrigger.threshold").ToObject<int>())
+                60
+                "First rule metric trigger threshold incorrect"
+            Expect.equal
+                ((rule1.SelectToken "metricTrigger.timeAggregation").ToObject<string>())
+                "Average"
+                "First rule metric trigger timeAggregation incorrect"
+            Expect.equal
+                ((rule1.SelectToken "metricTrigger.timeGrain").ToObject<string>())
+                "PT5M"
+                "First rule metric trigger timeGrain incorrect"
+            Expect.equal
+                ((rule1.SelectToken "metricTrigger.timeWindow").ToObject<string>())
+                "PT10M"
+                "First rule metric trigger timeWindow incorrect"
+            Expect.equal
+                ((rule1.SelectToken "scaleAction.cooldown").ToObject<string>())
+                "PT10M"
+                "First rule metric scaleAction cooldown incorrect"
+            Expect.equal
+                ((rule1.SelectToken "scaleAction.direction").ToObject<string>())
+                "Increase"
+                "First rule metric scaleAction direction incorrect"
+            Expect.equal
+                ((rule1.SelectToken "scaleAction.type").ToObject<string>())
+                "ChangeCount"
+                "First rule metric scaleAction type incorrect"
+            Expect.equal
+                ((rule1.SelectToken "scaleAction.value").ToObject<string>())
+                "1"
+                "First rule metric scaleAction value incorrect"
         }
     ]
