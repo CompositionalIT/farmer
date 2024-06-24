375253dbde417b18e8237b34584157eb2c6ca335
Meghaj <meghajsingh@microsoft.com>
2024-06-24 12:30:32 +0530

    remove

--- src/Farmer/Arm/AutoscaleSettings.fs
+++ src/Farmer/Arm/AutoscaleSettings.fs
@@ -11,7 +11,7 @@ let autoscaleSettings =
 
 // Have avoided SRTPs in the past but end up with a lot of repetitive code, so trying them.
 
-module internal Option =
+module private Option =
     let defaultUnchecked<'t> = Option.defaultValue Unchecked.defaultof<'t>
 
     let inline toArmJson resourceOpt =
@@ -19,7 +19,7 @@ module internal Option =
         |> Option.map (fun resource -> (^Resource: (member ToArmJson: 't) resource))
         |> defaultUnchecked
 
-module internal List =
+module private List =
     let inline mapToArmJson (list: List<_>) =
         if list.IsEmpty then
             null
@@ -33,303 +33,345 @@ module internal List =
 // https://chat.openai.com/share/d6ef3c5e-869c-469d-bf8b-e6488675407c
 module Json =
 
-    type Email = {
-        customEmails: string seq
-        sendToSubscriptionAdministrator: bool
-        sendToSubscriptionCoAdministrators: bool
-    }
+    type Email =
+        {
+            customEmails: string seq
+            sendToSubscriptionAdministrator: bool
+            sendToSubscriptionCoAdministrators: bool
+        }
 
     type Webhook = { properties: obj; serviceUri: string }
 
-    type Notification = {
-        email: Email
-        operation: string
-        webhooks: Webhook seq
-    }
+    type Notification =
+        {
+            email: Email
+            operation: string
+            webhooks: Webhook seq
+        }
 
-    type Capacity = {
-        ``default``: string
-        maximum: string
-        minimum: string
-    }
+    type Capacity =
+        {
+            ``default``: string
+            maximum: string
+            minimum: string
+        }
 
-    type Schedule = {
-        days: string list
-        hours: int list
-        minutes: int list
-        timeZone: string
-    }
+    type Schedule =
+        {
+            days: string list
+            hours: int list
+            minutes: int list
+            timeZone: string
+        }
 
-    type Dimension = {
-        dimensionName: string
-        operator: string
-        values: string list
-    }
+    type Dimension =
+        {
+            dimensionName: string
+            operator: string
+            values: string list
+        }
 
-    type MetricTrigger = {
-        dimensions: Dimension seq
-        dividePerInstance: bool
-        metricName: string
-        metricNamespace: string
-        metricResourceLocation: string
-        metricResourceUri: string
-        operator: string
-        statistic: string
-        threshold: int
-        timeAggregation: string
-        timeGrain: string
-        timeWindow: string
-    }
+    type MetricTrigger =
+        {
+            dimensions: Dimension seq
+            dividePerInstance: bool
+            metricName: string
+            metricNamespace: string
+            metricResourceLocation: string
+            metricResourceUri: string
+            operator: string
+            statistic: string
+            threshold: int
+            timeAggregation: string
+            timeGrain: string
+            timeWindow: string
+        }
 
-    type ScaleAction = {
-        cooldown: string
-        direction: string
-        ``type``: string
-        value: string
-    }
+    type ScaleAction =
+        {
+            cooldown: string
+            direction: string
+            ``type``: string
+            value: string
+        }
 
-    type Rule = {
-        metricTrigger: MetricTrigger
-        scaleAction: ScaleAction
-    }
+    type Rule =
+        {
+            metricTrigger: MetricTrigger
+            scaleAction: ScaleAction
+        }
 
-    type Recurrence = {
-        frequency: string
-        schedule: Schedule
-    }
+    type Recurrence =
+        {
+            frequency: string
+            schedule: Schedule
+        }
 
-    type FixedDate = {
-        ``end``: string
-        start: string
-        timeZone: string
-    }
+    type FixedDate =
+        {
+            ``end``: string
+            start: string
+            timeZone: string
+        }
 
-    type Profile = {
-        capacity: Capacity
-        fixedDate: FixedDate
-        name: string
-        recurrence: Recurrence
-        rules: Rule seq
-    }
+    type Profile =
+        {
+            capacity: Capacity
+            fixedDate: FixedDate
+            name: string
+            recurrence: Recurrence
+            rules: Rule seq
+        }
 
-    type PredictiveAutoscalePolicy = {
-        scaleLookAheadTime: string
-        scaleMode: string
-    }
+    type PredictiveAutoscalePolicy =
+        {
+            scaleLookAheadTime: string
+            scaleMode: string
+        }
 
-    type AutoscaleSettingsProperties = {
-        enabled: bool
-        name: string
-        notifications: Notification seq
-        predictiveAutoscalePolicy: PredictiveAutoscalePolicy
-        profiles: Profile seq
-        targetResourceLocation: string
-        targetResourceUri: string
-    }
+    type AutoscaleSettingsProperties =
+        {
+            enabled: bool
+            name: string
+            notifications: Notification seq
+            predictiveAutoscalePolicy: PredictiveAutoscalePolicy
+            profiles: Profile seq
+            targetResourceLocation: string
+            targetResourceUri: string
+        }
 
 open Json
 
-type Email = {
-    CustomEmails: string list
-    SendToSubscriptionAdministrator: bool
-    SendToSubscriptionCoAdministrators: bool
-} with
-
-    member this.ToArmJson = {
-        customEmails = this.CustomEmails
-        sendToSubscriptionAdministrator = this.SendToSubscriptionAdministrator
-        sendToSubscriptionCoAdministrators = this.SendToSubscriptionCoAdministrators
+type Email =
+    {
+        CustomEmails: string list
+        SendToSubscriptionAdministrator: bool
+        SendToSubscriptionCoAdministrators: bool
     }
 
-type Webhook = {
-    Properties: obj
-    ServiceUri: Uri
-} with
+    member this.ToArmJson =
+        {
+            customEmails = this.CustomEmails
+            sendToSubscriptionAdministrator = this.SendToSubscriptionAdministrator
+            sendToSubscriptionCoAdministrators = this.SendToSubscriptionCoAdministrators
+        }
 
-    member this.ToArmJson = {
-        properties = this.Properties
-        serviceUri = this.ServiceUri.AbsoluteUri
+type Webhook =
+    {
+        Properties: obj
+        ServiceUri: Uri
     }
 
-type Notification = {
-    Email: Email
-    Webhooks: Webhook list
-} with
+    member this.ToArmJson =
+        {
+            properties = this.Properties
+            serviceUri = this.ServiceUri.AbsoluteUri
+        }
 
-    member this.ToArmJson = {
-        email = this.Email.ToArmJson
-        operation = "Scale"
-        webhooks = this.Webhooks |> List.mapToArmJson
+type Notification =
+    {
+        Email: Email
+        Webhooks: Webhook list
     }
 
+    member this.ToArmJson =
+        {
+            email = this.Email.ToArmJson
+            operation = "Scale"
+            webhooks = this.Webhooks |> List.mapToArmJson
+        }
 
-type Capacity = {
-    Default: int
-    Maximum: int
-    Minimum: int
-} with
 
-    member this.ToArmJson = {
-        ``default`` = string this.Default
-        maximum = string this.Maximum
-        minimum = string this.Minimum
+type Capacity =
+    {
+        Default: int
+        Maximum: int
+        Minimum: int
     }
 
-type Schedule = {
-    Days: DayOfWeek list
-    Hours: int list
-    Minutes: int list
-    TimeZone: string
-} with
+    member this.ToArmJson =
+        {
+            ``default`` = string this.Default
+            maximum = string this.Maximum
+            minimum = string this.Minimum
+        }
 
-    member this.ToArmJson = {
-        days = this.Days |> List.map string
-        hours = this.Hours
-        minutes = this.Minutes
-        timeZone = this.TimeZone
+type Schedule =
+    {
+        Days: DayOfWeek list
+        Hours: int list
+        Minutes: int list
+        TimeZone: string
     }
 
-type Dimension = {
-    DimensionName: string
-    Operator: DimensionOperator
-    Values: string list
-} with
+    member this.ToArmJson =
+        {
+            days = this.Days |> List.map string
+            hours = this.Hours
+            minutes = this.Minutes
+            timeZone = this.TimeZone
+        }
 
-    member this.ToArmJson = {
-        dimensionName = this.DimensionName
-        operator = this.Operator.ArmValue
-        values = this.Values
+type Dimension =
+    {
+        DimensionName: string
+        Operator: DimensionOperator
+        Values: string list
     }
 
-type MetricTrigger = {
-    Dimensions: Dimension list
-    DividePerInstance: bool option
-    MetricName: string
-    MetricNamespace: string option
-    MetricResourceLocation: string option
-    MetricResourceUri: ResourceId
-    Operator: MetricTriggerOperator
-    Statistic: MetricTriggerStatistic
-    Threshold: int
-    TimeAggregation: MetricTriggerTimeAggregation
-    TimeGrain: TimeSpan // Between 12 hours and 1 minute
-    TimeWindow: TimeSpan // Between 12 hours and 5 minutes
-} with
+    member this.ToArmJson =
+        {
+            dimensionName = this.DimensionName
+            operator = this.Operator.ArmValue
+            values = this.Values
+        }
 
-    member this.ToArmJson = {
-        dimensions = this.Dimensions |> List.mapToArmJson
-        dividePerInstance = this.DividePerInstance |> Option.defaultValue false
-        metricName = this.MetricName
-        metricNamespace = this.MetricNamespace |> Option.toObj
-        metricResourceLocation = this.MetricResourceLocation |> Option.toObj
-        metricResourceUri = this.MetricResourceUri.Eval()
-        operator = this.Operator.ArmValue
-        statistic = this.Statistic.ArmValue
-        threshold = this.Threshold
-        timeAggregation = this.TimeAggregation.ArmValue
-        timeGrain = XmlConvert.ToString this.TimeGrain
-        timeWindow = XmlConvert.ToString this.TimeWindow
+type MetricTrigger =
+    {
+        Dimensions: Dimension list
+        DividePerInstance: bool option
+        MetricName: string
+        MetricNamespace: string option
+        MetricResourceLocation: string option
+        MetricResourceUri: ResourceId
+        Operator: MetricTriggerOperator
+        Statistic: MetricTriggerStatistic
+        Threshold: int
+        TimeAggregation: MetricTriggerTimeAggregation
+        TimeGrain: TimeSpan // Between 12 hours and 1 minute
+        TimeWindow: TimeSpan // Between 12 hours and 5 minutes
     }
 
-type ScaleAction = {
-    Cooldown: TimeSpan // from one week to one minute
-    Direction: ScaleActionDirection
-    Type: ScaleActionType
-    Value: int
-} with
+    member this.ToArmJson =
+        {
+            dimensions = this.Dimensions |> List.mapToArmJson
+            dividePerInstance = this.DividePerInstance |> Option.defaultValue false
+            metricName = this.MetricName
+            metricNamespace = this.MetricNamespace |> Option.toObj
+            metricResourceLocation = this.MetricResourceLocation |> Option.toObj
+            metricResourceUri = this.MetricResourceUri.Eval()
+            operator = this.Operator.ArmValue
+            statistic = this.Statistic.ArmValue
+            threshold = this.Threshold
+            timeAggregation = this.TimeAggregation.ArmValue
+            timeGrain = XmlConvert.ToString this.TimeGrain
+            timeWindow = XmlConvert.ToString this.TimeWindow
+        }
 
-    member this.ToArmJson = {
-        cooldown = XmlConvert.ToString this.Cooldown
-        direction = this.Direction.ArmValue
-        ``type`` = this.Type.ArmValue
-        value = string this.Value
+type ScaleAction =
+    {
+        Cooldown: TimeSpan // from one week to one minute
+        Direction: ScaleActionDirection
+        Type: ScaleActionType
+        Value: int
     }
 
-type Rule = {
-    MetricTrigger: MetricTrigger
-    ScaleAction: ScaleAction
-} with
+    member this.ToArmJson =
+        {
+            cooldown = XmlConvert.ToString this.Cooldown
+            direction = this.Direction.ArmValue
+            ``type`` = this.Type.ArmValue
+            value = string this.Value
+        }
 
-    member this.ToArmJson = {
-        metricTrigger = this.MetricTrigger.ToArmJson
-        scaleAction = this.ScaleAction.ToArmJson
+type Rule =
+    {
+        MetricTrigger: MetricTrigger
+        ScaleAction: ScaleAction
     }
 
-type Recurrence = {
-    Frequency: string
-    Schedule: Schedule
-} with
+    member this.ToArmJson =
+        {
+            metricTrigger = this.MetricTrigger.ToArmJson
+            scaleAction = this.ScaleAction.ToArmJson
+        }
 
-    member this.ToArmJson = {
-        frequency = this.Frequency
-        schedule = this.Schedule.ToArmJson
+type Recurrence =
+    {
+        Frequency: string
+        Schedule: Schedule
     }
 
-type FixedDate = {
-    End: DateTimeOffset
-    Start: DateTimeOffset
-    TimeZone: string option
-} with
+    member this.ToArmJson =
+        {
+            frequency = this.Frequency
+            schedule = this.Schedule.ToArmJson
+        }
 
-    member this.ToArmJson = {
-        ``end`` = this.End.ToString("o")
-        start = this.Start.ToString("o")
-        timeZone = this.TimeZone |> Option.toObj
+type FixedDate =
+    {
+        End: DateTimeOffset
+        Start: DateTimeOffset
+        TimeZone: string option
     }
 
-type Profile = {
-    Capacity: Capacity
-    FixedDate: FixedDate option
-    Name: string
-    Recurrence: Recurrence option
-    Rules: Rule list
-} with
+    member this.ToArmJson =
+        {
+            ``end`` = this.End.ToString("o")
+            start = this.Start.ToString("o")
+            timeZone = this.TimeZone |> Option.toObj
+        }
 
-    member this.ToArmJson = {
-        capacity = this.Capacity.ToArmJson
-        fixedDate = this.FixedDate |> Option.toArmJson
-        name = this.Name
-        recurrence = this.Recurrence |> Option.toArmJson
-        rules = this.Rules |> List.mapToArmJson
+type Profile =
+    {
+        Capacity: Capacity
+        FixedDate: FixedDate option
+        Name: string
+        Recurrence: Recurrence option
+        Rules: Rule list
     }
 
-type PredictiveAutoscalePolicy = {
-    ScaleLookAheadTime: string
-    ScaleMode: string
-} with
+    member this.ToArmJson =
+        {
+            capacity = this.Capacity.ToArmJson
+            fixedDate = this.FixedDate |> Option.toArmJson
+            name = this.Name
+            recurrence = this.Recurrence |> Option.toArmJson
+            rules = this.Rules |> List.mapToArmJson
+        }
 
-    member this.ToArmJson = {
-        scaleLookAheadTime = this.ScaleLookAheadTime
-        scaleMode = this.ScaleMode
+type PredictiveAutoscalePolicy =
+    {
+        ScaleLookAheadTime: string
+        ScaleMode: string
     }
 
-type AutoscaleSettingsProperties = {
-    Enabled: bool
-    Name: string
-    Notifications: Notification list
-    PredictiveAutoscalePolicy: PredictiveAutoscalePolicy option
-    Profiles: Profile list
-    TargetResourceLocation: string
-    TargetResourceUri: LinkedResource
-} with
+    member this.ToArmJson =
+        {
+            scaleLookAheadTime = this.ScaleLookAheadTime
+            scaleMode = this.ScaleMode
+        }
 
-    member this.ToArmJson = {
-        enabled = this.Enabled
-        name = this.Name
-        notifications = this.Notifications |> List.mapToArmJson
-        predictiveAutoscalePolicy = this.PredictiveAutoscalePolicy |> Option.toArmJson
-        profiles = this.Profiles |> List.mapToArmJson
-        targetResourceLocation = this.TargetResourceLocation
-        targetResourceUri = this.TargetResourceUri.ResourceId.Eval()
+type AutoscaleSettingsProperties =
+    {
+        Enabled: bool
+        Name: string
+        Notifications: Notification list
+        PredictiveAutoscalePolicy: PredictiveAutoscalePolicy option
+        Profiles: Profile list
+        TargetResourceLocation: string
+        TargetResourceUri: LinkedResource
     }
 
-type AutoscaleSettings = {
-    Name: ResourceName
-    Location: Location
-    Tags: Map<string, string>
-    Properties: AutoscaleSettingsProperties
-    Dependencies: ResourceId Set
-} with
+    member this.ToArmJson =
+        {
+            enabled = this.Enabled
+            name = this.Name
+            notifications = this.Notifications |> List.mapToArmJson
+            predictiveAutoscalePolicy = this.PredictiveAutoscalePolicy |> Option.toArmJson
+            profiles = this.Profiles |> List.mapToArmJson
+            targetResourceLocation = this.TargetResourceLocation
+            targetResourceUri = this.TargetResourceUri.ResourceId.Eval()
+        }
+
+type AutoscaleSettings =
+    {
+        Name: ResourceName
+        Location: Location
+        Tags: Map<string, string>
+        Properties: AutoscaleSettingsProperties
+        Dependencies: ResourceId Set
+    }
 
     interface IArmResource with
         member this.JsonModel =
@@ -337,9 +379,8 @@ type AutoscaleSettings = {
                 this.Dependencies
                 |> LinkedResource.addToSetIfManaged this.Properties.TargetResourceUri
 
-            {|
-                autoscaleSettings.Create(this.Name, this.Location, dependsOn = dependencies, tags = this.Tags) with
-                    properties = this.Properties.ToArmJson
+            {| autoscaleSettings.Create(this.Name, this.Location, dependsOn = dependencies, tags = this.Tags) with
+                properties = this.Properties.ToArmJson
             |}
 
         member this.ResourceId = autoscaleSettings.resourceId this.Name
