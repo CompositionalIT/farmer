458ef8a805d1dc92c69a3a96df052751d977ed19
Andrii Chebukin <XperiAndri@Outlook.com>
2024-10-25 00:23:29 +0400

    Fixed extension methods usage

--- src/Farmer/Common.fs
+++ src/Farmer/Common.fs
@@ -1556,6 +1556,12 @@ module Storage =
         | Container
         | Blob
 
+        member this.ArmValue =
+            match this with
+            | Private -> "None"
+            | Container -> "Container"
+            | Blob -> "Blob"
+
     /// The type of action to take when defining a lifecycle policy.
     type LifecyclePolicyAction =
         | CoolAfter of int<Days>
