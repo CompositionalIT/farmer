4d013c8aabe6bf1fe01d7032a25c24a438f29baf
Michelle Li <michellli@microsoft.com>
2025-08-06 22:39:17 +0000

    more tests for prometheus rule group

--- src/Tests/PrometheusRuleGroup.fs
+++ src/Tests/PrometheusRuleGroup.fs
@@ -47,9 +47,14 @@ let tests =
                     .SelectToken("resources[?(@.name=='myGroup')].properties.rules[1].labels")
                     .ToString()
 
+            let actualScopes =
+                jobj.SelectToken("resources[?(@.name=='myGroup')].properties.scopes").ToString()
+
             Expect.isNotNull actualRules "Expected rules is not null"
             Expect.isTrue (actualRule1.Contains("myRecord")) "Expected rule with record 'myRecord' exists"
             Expect.isTrue (actualRule2.Contains("workload_type")) "Expected rule with label 'workload_type' exists"
+            Expect.isNotNull actualScopes "Expected scopes is not null"
+            Expect.isTrue (actualScopes.Contains(monitorAccountId.Eval())) "Expected monitor workspace id in scopes"
         }
 
         test "Create prometheus rule group with rules and set interval" {
@@ -120,5 +125,69 @@ let tests =
                     }
                     |> ignore)
                 (sprintf "Should have thrown an exception for not specifying rules")
+        }
+
+        test "Enable prometheus rule group" {
+            let myRule1 = prometheusRule {
+                record (Some "myRecord")
+                expression "up == 1"
+            }
+
+            let monitoringAccountType =
+                ResourceType("Microsoft.Monitor/accounts", "2025-05-03-preview")
+
+            let monitorAccountId =
+                ResourceId.create (monitoringAccountType, ResourceName "monitorAccount")
+
+            let myGroup = prometheusRuleGroup {
+                name "myGroup"
+                add_rules [ myRule1 ]
+                azure_monitor_workspace_id monitorAccountId
+                enable_rule_group
+            }
+
+            let template = arm { add_resources [ myGroup ] }
+            let jsn = template.Template |> Writer.toJson
+            let jobj = jsn |> Newtonsoft.Json.Linq.JObject.Parse
+
+            let actualEnabled =
+                jobj
+                    .SelectToken("resources[?(@.name=='myGroup')].properties.enabled")
+                    .ToString()
+                    .ToLower()
+
+            Expect.isTrue (actualEnabled = "true") "Expected rule group to be enabled"
+        }
+
+        test "Edit cluster name for prometheus rule group" {
+            let myRule1 = prometheusRule {
+                record (Some "myRecord")
+                expression "up == 1"
+            }
+
+            let monitoringAccountType =
+                ResourceType("Microsoft.Monitor/accounts", "2025-05-03-preview")
+
+            let monitorAccountId =
+                ResourceId.create (monitoringAccountType, ResourceName "monitorAccount")
+
+            let myGroup = prometheusRuleGroup {
+                name "myGroup"
+                add_rules [ myRule1 ]
+                azure_monitor_workspace_id monitorAccountId
+                enable_rule_group
+                cluster_name (Some(ResourceName "myCluster"))
+            }
+
+            let template = arm { add_resources [ myGroup ] }
+            let jsn = template.Template |> Writer.toJson
+            let jobj = jsn |> Newtonsoft.Json.Linq.JObject.Parse
+
+            let actualClusterName =
+                jobj
+                    .SelectToken("resources[?(@.name=='myGroup')].properties.clusterName")
+                    .ToString()
+
+            Expect.isTrue (actualClusterName = "myCluster") "Expected cluster name to be myCluster"
         }
     ]
