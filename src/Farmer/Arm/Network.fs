6d568440444578cd1e92c4cbff64cc76cf35a69b
Dave Curylo <dave@curylo.org>
2025-08-07 16:12:56 -0400

    NAT Gateways: set the SKU of NAT Gateway to deploy.

--- src/Farmer/Arm/Network.fs
+++ src/Farmer/Arm/Network.fs
@@ -1,13 +1,12 @@
 [<AutoOpen>]
 module Farmer.Arm.Network
 
-open System.Net.Mail
 open Farmer
-open Farmer.Arm
 open Farmer.ExpressRoute
 open Farmer.Network
 open Farmer.Route
 open Farmer.RouteServer
+open Farmer.Storage
 open Farmer.VirtualNetworkGateway
 
 let connections = ResourceType("Microsoft.Network/connections", "2020-04-01")
@@ -28,10 +27,10 @@ let networkProfiles =
     ResourceType("Microsoft.Network/networkProfiles", "2020-04-01")
 
 let publicIPAddresses =
-    ResourceType("Microsoft.Network/publicIPAddresses", "2024-05-01")
+    ResourceType("Microsoft.Network/publicIPAddresses", "2024-07-01")
 
 let publicIPPrefixes =
-    ResourceType("Microsoft.Network/publicIPPrefixes", "2024-05-01")
+    ResourceType("Microsoft.Network/publicIPPrefixes", "2024-07-01")
 
 let serviceEndpointPolicies =
     ResourceType("Microsoft.Network/serviceEndpointPolicies", "2020-07-01")
@@ -48,7 +47,7 @@ let virtualNetworkGateways =
 let localNetworkGateways =
     ResourceType("Microsoft.Network/localNetworkGateways", "")
 
-let natGateways = ResourceType("Microsoft.Network/natGateways", "2021-08-01")
+let natGateways = ResourceType("Microsoft.Network/natGateways", "2024-07-01")
 
 let privateEndpoints =
     ResourceType("Microsoft.Network/privateEndpoints", "2021-05-01")
@@ -164,7 +163,7 @@ type RouteTable = {
 type RouteServer = {
     Name: ResourceName
     Location: Location
-    Sku: Sku
+    Sku: RouteServer.Sku
     AllowBranchToBranchTraffic: FeatureFlag
     HubRoutingPreference: HubRoutingPreference
     Tags: Map<string, string>
@@ -1028,6 +1027,7 @@ type NatGateway = {
     IdleTimeout: int<Minutes>
     PublicIpAddresses: LinkedResource list
     PublicIpPrefixes: LinkedResource list
+    Sku: NatGateway.Sku
     Tags: Map<string, string>
 } with
 
@@ -1046,7 +1046,7 @@ type NatGateway = {
 
             {|
                 natGateways.Create(this.Name, this.Location, dependsOn = dependencies, tags = this.Tags) with
-                    sku = {| name = "Standard" |}
+                    sku = {| name = this.Sku.ArmValue |}
                     properties = {|
                         idleTimeoutInMinutes = this.IdleTimeout
                         publicIpAddresses = this.PublicIpAddresses |> List.map LinkedResource.AsIdObject
