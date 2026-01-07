a9a697fe21b82cb64bc5da6b2bbc93f7ec12904a
Ryan Palmer <ryanpalmermultimedia@gmail.com>
2026-01-07 14:05:42 +0000

    Docs

--- src/Tests/LogAnalytics.fs
+++ src/Tests/LogAnalytics.fs
@@ -4,7 +4,6 @@ open Expecto
 open Farmer
 open Farmer.Arm
 open Farmer.Builders
-open Farmer.Helpers
 open Microsoft.Azure.Management.OperationalInsights
 open Microsoft.Azure.Management.OperationalInsights.Models
 open Microsoft.Rest
